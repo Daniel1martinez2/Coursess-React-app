5786e1c75cb78d106bb7bb51ed0e46cfa4b0fcaa
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-05-17 18:30:03 -0500

    setting styles dynamically 🍔
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,20 +5,30 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
+  const [isValid, setIsValid]=useState(true); 
+  const [label, setLabel] = useState('Course Goal'); 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    if(event.target.value.trim() !== ''){
+      setLabel('Course Goal')
+      setIsValid(true); 
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim() === ''){
+      setIsValid(false); 
+      setLabel('Empty Field')
+      return; 
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      <div className={`form-control ${isValid? '':'invalid'}`}>
+        <label>{label}</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
